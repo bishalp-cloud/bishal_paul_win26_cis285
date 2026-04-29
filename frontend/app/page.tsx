7993367dc91122fff76c18a4eada25f514e27ada
bishalp-cloud <bishalp@umich.edu>
2026-04-29 01:22:48 +0000

    removed frontend folder

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">
-        CIS 285 Final Exam
-      </h1>
-    </div>
-  );
-}

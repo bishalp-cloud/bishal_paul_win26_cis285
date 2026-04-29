e1bc81db5a6b05787be06a165317fb9ebd4f53fa
bishalp-cloud <bishalp@umich.edu>
2026-04-29 00:52:02 +0000

    CIS 285 Final Exam completed

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM mcr.microsoft.com/devcontainers/universal:latest
+
+RUN npm install -g create-next-app express-generator
---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">
+        CIS 285 Final Exam
+      </h1>
+    </div>
+  );
+}
